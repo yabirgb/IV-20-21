46d8c969e03570614a8ec04b6787bcbd9d6c6442
JJ Merelo <jjmerelo@gmail.com>
2020-12-09 14:07:00 +0100

    [skip ci] Nombre clave YAML correcta

--- .github/workflows/procesa_yaml.sh
+++ .github/workflows/procesa_yaml.sh
@@ -3,8 +3,8 @@
 echo "Procesando fichero iv.yaml"
 lenguaje=$(ysh -f iv.yaml -q "lenguaje")
 [[ $lenguaje =~ \= ]] && echo "«lenguaje» no es una cadena" && exit 1
-fichero_tareas=$(ysh -f iv.yaml -q "fichero_tareas")
-[[ $fichero_tareas =~ \= ]] && echo "«fichero_tareas» no es una cadena" && exit 2
+fichero_tareas=$(ysh -f iv.yaml -q "taskfile")
+[[ $fichero_tareas =~ \= ]] && echo "«taskfile» no es una cadena" && exit 2
 make=$(ysh -f iv.yaml -q "make")
 [[ $make =~ \= ]] && echo "«make» no es una cadena" && exit 2
 echo "IV_LENGUAJE=$lenguaje" >> IV_CONFIG
