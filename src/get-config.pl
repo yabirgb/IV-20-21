370f2e8b3534429139315b65629321767f52e299
JJ Merelo <jjmerelo@gmail.com>
2021-01-14 13:09:14 +0100

    Añade nuevo test

---
+++ src/get-config.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use v5.14;
+
+use Test::More;
+
+use JSON;
+use YAML qw(LoadFile);
+use GitHub::Actions;
+
+use constant KEYS => qw(entidad lenguaje test taskfil URL make PaaS recurso);
+
+my $iv = LoadFile("iv.yaml");
+ok( $iv, "Fichero de configuración para corrección iv.yaml cargado correctamente" );
+
+for my $c ( KEYS ) {
+  my $output = (ref $iv->{$c} )?to_json( $iv->{$c} ):$iv->{$c};
+  set_output( $c, $output );
+}
+
